73112d2b95a1cf103c95c5cd530b034b3a339edf
João <joao.felipe@tmh.com.br>
2021-05-01 08:09:49 -0300

    Creating Setting model

---
+++ models/Setting.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const SettingSchema = new mongoose.Schema({
+    key: {
+        type: String,
+        required: true
+        unique: true
+    },
+    value: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = Setting = mongoose.model('setting', SettingSchema);
