4512cc60df286203754b5b9d0eb9b9005580e22b
João <joao.felipe@tmh.com.br>
2021-05-01 08:12:30 -0300

    Removing unused model import from user routes file

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 
-const User = require('../../models/User')
 const UserController = require('../../controllers/User')
 const validator = require('../../validators/User')
 
