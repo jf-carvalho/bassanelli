1d015236fa8ca3252f1d677c34aa1944eb7fe2f7
João <joao.felipe@tmh.com.br>
2021-05-01 08:11:16 -0300

    Renaming user route and adding setting route

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ app.use(express.json({extended: false}));
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define routes
-app.use('/api/users', require('./routes/api/users'));
+app.use('/api/user', require('./routes/api/user'));
 app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/setting', require('./routes/api/setting'));
 
 const PORT = process.env.PORT || 5000;
 
