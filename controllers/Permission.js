5e7407dac16db0a87c3e85ac7a801f192dd976fc
João <joao.felipe@tmh.com.br>
2021-05-08 09:56:46 -0300

    Grouping permissions on GET Permissions

--- controllers/Permission.js
+++ controllers/Permission.js
@@ -10,13 +10,24 @@ const Permission = require('../models/Permission')
 class PermissionController{
 	async index(req, res) {
 		 try{
-	        const Permissions = await Permission.find();
-	        res.json({ Permissions });
+	        let permissions = await Permission.find().sort({'group' : 1}).distinct('group');
+
+	        permissions = await Promise.all(permissions.map(async (name) => {
+	        	let group = {};
+
+	        	group.group = name;
+	        	group.permissions = await Permission.find({'group': name}).sort({'name': -1}).select('-group')
+
+	        	return group;
+	        }))
+
+	        res.json({ permissions });
 	    }catch(err){
 	        console.log(err);
 	        res.status('500').send(config.get("errors.server"));
 	    }
 	}
 }
+
 
 module.exports = new PermissionController();
